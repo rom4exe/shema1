324d658f54de2c1166df4b65af3870eeb2b10e27
rom4exe <135598812+rom4exe@users.noreply.github.com>
2023-09-13 22:04:40 +0800

    Update SELECT.sql

--- DZ3/SELECT.sql
+++ DZ3/SELECT.sql
@@ -1,16 +1,17 @@
- SELECT name_track название, duration длительность FROM track 
+SELECT name_track название, duration длительность FROM track 
  where duration = (select MAX(duration) from  track);
 
 SELECT name_track название, duration длительность FROM track 
  where duration >= '00:03:30';
 
-SELECT name_collection сборник FROM Сollection 
+SELECT name_collection сборник FROM Collection 
  where year_release >= 2018 and year_release <= 2020;
 
 select name_executor from executor where name_executor not like '% %';
 
 SELECT name_track название FROM track 
  where name_track like '%My%';
+
 select name_genre жанр, COUNT(id_executor) количество_исполнителей_в_жанре FROM executorgenre e
 join genre g on g.id = e.id_genre
 GROUP BY name_genre;
